b673113efddc6fd110ef291e72b95edb1792853e
Jasper den Ouden <o.jasper@gmail.com>
2014-03-21 11:14:05 +0100

    Removed a flake8 warning on sim.py

--- lib/sim.py
+++ lib/sim.py
@@ -111,7 +111,7 @@ class Simulation(object):
 
         if method_name is None:
             method_name = inspect.stack()[1][3]
-        
+
         logging.info("RUN %s: %s" % (method_name, tx))
 
         contract.txs = []
