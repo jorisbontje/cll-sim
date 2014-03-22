deea13a9c3c62e8ac4a7bd08b88fba5723d091f9
Jasper den Ouden <o.jasper@gmail.com>
2014-03-22 22:45:59 +0100

    Added contract.txs testing. Actually had to change max -> min which was a mistake.. Not yet checked coding conventions.

--- examples/lockin-escrow.py
+++ examples/lockin-escrow.py
@@ -1,6 +1,7 @@
 from sim import Block, Contract, Simulation, Tx, mktx, stop
 from random import random
 import inspect
+import logging
 
 # Constants to modify before contract creation.
 CUSTOMER = "carol"
@@ -65,11 +66,11 @@ class LockinEscrow(Contract):
                 refund =  contract.storage[I_PAID]
                 refund += contract.storage[I_PAID]*MIN_BALANCE/contract.storage[I_TOTAL]
 
-                mktx(customer, max(refund, contract.storage[I_TOTAL]+MIN_BALANCE), 0, [])
-                contract.storage[I_CUSTOMER] = 0
-                contract.storage[I_TOTAL] = 0
+                mktx(customer, min(refund, contract.storage[I_TOTAL]+MIN_BALANCE), 0, [])
+                contract.storage[I_CUSTOMER]  = 0
+                contract.storage[I_TOTAL]     = 0
                 contract.storage[I_INCENTIVE] = 0
-                contract.storage[I_PAID] = 0
+                contract.storage[I_PAID]      = 0
                 stop("Customer refunded")
             stop("Merchant topping up")
 
@@ -78,8 +79,9 @@ class LockinEscrow(Contract):
             if tx.datan == 1 and tx.data[0] == C_SATISFIED:
                 if contract.storage[I_PAID] <= contract.storage[I_TOTAL]:
                     stop("Customer didnt pay enough")
-                mktx(MERCHANT, contract.storage[I_PAID], 0, [])
-                mktx(customer, contract.storage[I_INCENTIVE], 0, [])
+                incentive = contract.storage[I_INCENTIVE]
+                mktx(MERCHANT, contract.storage[I_PAID]-incentive, 0, [])
+                mktx(customer, incentive, 0, [])
                 contract.storage[I_CUSTOMER]  = 0
                 contract.storage[I_TOTAL]     = 0
                 contract.storage[I_INCENTIVE] = 0
@@ -88,12 +90,24 @@ class LockinEscrow(Contract):
             stop("Customer paid(part)")
         stop("Donation")
 
+def random_incentive():
+    return INCENTIVE*(0.1 + random())
 
 class LockinEscrowRun(Simulation):
 
     contract = LockinEscrow()
     block = Block()
 
+    total     = 0
+    incentive = 0
+    paid      = 0
+
+    def reset(self):
+        self.contract  = LockinEscrow()
+        self.total     = 0
+        self.incentive = 0
+        self.paid      = 0
+        
     def run_tx(self, value=0, sender="", data=[]):
         self.run(Tx(value=value, sender=sender, data=data), self.contract, self.block,
                  method_name=inspect.stack()[1][3])
@@ -108,60 +122,84 @@ class LockinEscrowRun(Simulation):
         self.stopped == "Below funds of operation"
 
     def test_merchant_allow(self):
+        #Test intended when contract not busy.
+        assert self.contract.storage[I_CUSTOMER] == 0 
+        
         self.block.set_account_balance(self.contract.address, MIN_BALANCE)
+        self.incentive = random_incentive()
+        self.total     = PRICE + self.incentive
         self.run_tx(sender=MERCHANT, value=MIN_BALANCE + MIN_FEE,
-                    data=[C_ALLOW, CUSTOMER, TOTAL, INCENTIVE])
-        self.stopped == "Customer allowed"
-        assert self.contract.storage[I_CUSTOMER] == CUSTOMER
-        assert self.contract.storage[I_TOTAL] == TOTAL
-        assert self.contract.storage[I_INCENTIVE] == INCENTIVE
-        assert self.contract.storage[I_PAID] == 0
+                    data=[C_ALLOW, CUSTOMER, self.total, self.incentive])
+        assert self.stopped == "Customer allowed"
+        assert self.contract.storage[I_CUSTOMER]  == CUSTOMER
+        assert self.contract.storage[I_TOTAL]     == self.total
+        assert self.contract.storage[I_INCENTIVE] == self.incentive
+        assert self.contract.storage[I_PAID]      == 0
 
     def test_customer_change_blocked(self):
+        r_incentive = random_incentive()
         self.run_tx(sender=MERCHANT, value=MIN_BALANCE + MIN_FEE,
-                    data=[C_ALLOW, CUSTOMER, TOTAL, INCENTIVE])
-        self.stopped == "Customer change blocked"
+                    data=[C_ALLOW, CUSTOMER, 2*TOTAL, r_incentive])
+        assert self.stopped == "Customer change blocked"
+        assert self.contract.storage[I_TOTAL]     == self.total
+        assert self.contract.storage[I_INCENTIVE] == self.incentive
 
     def test_customer_pay(self):
-        self.run_tx(sender=CUSTOMER, value=random()*PRICE)
+        self.paid = random()*PRICE
+        self.run_tx(sender=CUSTOMER, value=self.paid)
         assert self.stopped == "Customer paid(part)"
 
     def test_customer_pay_too_little(self):
-        self.contract = LockinEscrow()
+        self.reset()
         self.test_merchant_allow()
-        self.run_tx(sender=CUSTOMER, value=random()*TOTAL, data=[C_SATISFIED])
+        self.paid = random()*TOTAL
+        self.run_tx(sender=CUSTOMER, value=self.paid + MIN_FEE, data=[C_SATISFIED])
         assert self.stopped == "Customer didnt pay enough"
+        assert len(self.contract.txs) == 0
 
     def assert_reset(self):
-        assert self.contract.storage[I_CUSTOMER] == 0
-        assert self.contract.storage[I_TOTAL] == 0
+        assert self.contract.storage[I_CUSTOMER]  == 0
+        assert self.contract.storage[I_TOTAL]     == 0
         assert self.contract.storage[I_INCENTIVE] == 0
-        assert self.contract.storage[I_PAID] == 0
+        assert self.contract.storage[I_PAID]      == 0
 
     def assert_happy(self):
         assert self.stopped == "Customer paid and happy"
         self.assert_reset()
 
     def test_customer_pay_and_happy(self):
-        self.contract = LockinEscrow()
+        self.reset()
         self.test_merchant_allow()
-        self.run_tx(sender=CUSTOMER, value=TOTAL+MIN_FEE+1, data=[C_SATISFIED])
+        self.paid = self.total + 1
+        self.run_tx(sender=CUSTOMER, value=self.paid + MIN_FEE, data=[C_SATISFIED])
+        assert self.contract.txs[0][0] == MERCHANT
+        assert self.contract.txs[0][1] == self.paid - self.incentive
+        assert self.contract.txs[1][0] == CUSTOMER
+        assert self.contract.txs[1][1] == self.incentive
         self.assert_happy()
 
     def test_customer_pay_part(self):
-        #  self.contract= LockinEscrow() #  not neccesary, already restarted by previous.
+        self.assert_reset()
         self.test_merchant_allow()
-        self.run_tx(sender=CUSTOMER, value=TOTAL+MIN_FEE)
+        self.paid = self.total
+        self.run_tx(sender=CUSTOMER, value=self.paid + MIN_FEE)
         assert self.stopped == "Customer paid(part)"  # (all, actually)
-        assert self.contract.storage[I_PAID] == TOTAL
+        assert self.contract.storage[I_PAID] == self.total
 
     def test_customer_happy(self):  # depends on the pay one being run first.
+        self.paid += 1
         self.run_tx(sender=CUSTOMER, value=MIN_FEE+1, data=[C_SATISFIED])
+        assert self.contract.txs[0][0] == MERCHANT
+        assert self.contract.txs[0][1] == self.paid - self.incentive
+        assert self.contract.txs[1][0] == CUSTOMER
+        assert self.contract.txs[1][1] == self.incentive
         self.assert_happy()
 
     def test_refund(self):
-        self.test_merchant_allow()
         self.test_customer_pay_part()
         self.run_tx(sender=MERCHANT, value=MIN_FEE, data=[C_REFUND])
         assert self.stopped == "Customer refunded"
+        assert self.contract.txs[0][0] == CUSTOMER
+        assert self.contract.txs[0][1] == min(self.paid*(1 + MIN_BALANCE/self.total),
+                                              self.total + MIN_BALANCE)
         self.assert_reset()
